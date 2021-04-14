04e6aec04066758ce95fca3d378493ccb5ba5975
adilett02 <adiletturdakunov2002@gmail.com>
2021-04-14 14:22:46 +0600

    .

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,86 @@
 $(function () {
+  //sticky-header
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 0) {
+      $('.header').addClass('header-sticky');
+    } else {
+      $('.header').removeClass('header-sticky');
+    }
+  });
 
-})
+  //scroll
+  // $('.menu-link').on('click', function () {
+  //   $('html, body').animate(
+  //     {
+  //       scrollTop: $($(this).attr('href')).offset().top - $('header').height() + 'px',
+  //     },
+  //     {
+  //       duration: 1000,
+  //       easing: 'easeInOutExpo',
+  //     },
+  //   );
+  // });
+  // $('.back-to-top').on('click', function () {
+  //   $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+  //   return false;
+  // });
+
+  const section = $('section'),
+    nav = $('.header'),
+    navHeight = nav.outerHeight();
+  $(window).on('scroll', function () {
+    const position = $(this).scrollTop();
+
+    section.each(function () {
+      const top = $(this).offset().top - navHeight - 100,
+        bottom = top + $(this).outerHeight();
+
+      if (position >= top && position <= bottom) {
+        nav.find('a').removeClass('active-link');
+        section.removeClass('active-section');
+
+        $(this).addClass('active-section');
+        nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active-link');
+      }
+    });
+  });
+
+  const mobilenav = $('.mobile-menu');
+
+  $(window).on('scroll', function () {
+    const position = $(this).scrollTop();
+
+    section.each(function () {
+      const top = $(this).offset().top - navHeight,
+        bottom = top + $(this).outerHeight();
+
+      if (position >= top && position <= bottom) {
+        mobilenav.find('a').removeClass('active-link');
+        section.removeClass('active-section');
+
+        $(this).addClass('active-section');
+        mobilenav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active-link');
+      }
+    });
+  });
+
+  $('body').on('click', '.toggle-menu', function (e) {
+    var $this = $(this);
+    e.preventDefault();
+
+    if ($('body').hasClass('mobile-body')) {
+      $('body').removeClass('mobile-body');
+      $('body').find('.toggle-menu').removeClass('active');
+    } else {
+      $('body').addClass('mobile-body');
+      $('body').find('.toggle-menu').addClass('active');
+    }
+  });
+
+  lightbox.option({
+    resizeDuration: 200,
+    wrapAround: true,
+    fadeDuration: 100,
+    resizeDuration: 100,
+  });
+});
--- gulpfile.js
+++ gulpfile.js
@@ -7,78 +7,70 @@ const autoprefixer = require('gulp-autoprefixer');
 const imagemin = require('gulp-imagemin');
 const del = require('del');
 
-
-
 function browsersync() {
-    browserSync.init({
-        server: {
-            baseDir: 'app/'
-        }
-    })
+  browserSync.init({
+    server: {
+      baseDir: 'app/',
+    },
+  });
 }
 
 function cleanDist() {
-    return del('dist')
+  return del('dist');
 }
 
 function images() {
-    return src('app/images/**/*')
-        .pipe(imagemin(
-            [
-                imagemin.gifsicle({ interlaced: true }),
-                imagemin.mozjpeg({ quality: 75, progressive: true }),
-                imagemin.optipng({ optimizationLevel: 5 }),
-                imagemin.svgo({
-                    plugins: [
-                        { removeViewBox: true },
-                        { cleanupIDs: false }
-                    ]
-                })
-            ]
-        ))
-        .pipe(dest('dist/images'))
+  return src('app/images/**/*')
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.mozjpeg({ quality: 75, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+        }),
+      ]),
+    )
+    .pipe(dest('dist/images'));
 }
 
 function scripts() {
-    return src([
-        'node_modules/jquery/dist/jquery.js',
-        'app/js/main.js'
-    ])
-        .pipe(concat('main.min.js'))
-        .pipe(uglify())
-        .pipe(dest('app/js'))
-        .pipe(browserSync.stream())
+  return src([
+    'node_modules/jquery/dist/jquery.js',
+    'node_modules/jquery-easing/dist/jquery.easing.1.3.umd.min.js',
+    'node_modules/lightbox2/dist/js/lightbox-plus-jquery.min.js',
+    'app/js/main.js',
+  ])
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js'))
+    .pipe(browserSync.stream());
 }
-
 
 function styles() {
-    return src('app/scss/style.scss')
-        .pipe(scss({ outputStyle: 'compressed' }))
-        .pipe(concat('style.min.css'))
-        .pipe(autoprefixer({
-            overrideBrowserslist: ['last 10 version'],
-            grid: true
-        }))
-        .pipe(dest('app/css'))
-        .pipe(browserSync.stream())
+  return src('app/scss/style.scss')
+    .pipe(scss({ outputStyle: 'compressed' }))
+    .pipe(concat('style.min.css'))
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ['last 10 version'],
+        grid: true,
+      }),
+    )
+    .pipe(dest('app/css'))
+    .pipe(browserSync.stream());
 }
-
 
 function build() {
-    return src([
-        'app/css/style.min.css',
-        'app/fonts/**/*',
-        'app/js/main.min.js',
-        'app/*.html'
-    ], { base: 'app' })
-        .pipe(dest('dist'))
+  return src(['app/css/style.min.css', 'app/fonts/**/*', 'app/js/main.min.js', 'app/*.html'], {
+    base: 'app',
+  }).pipe(dest('dist'));
 }
 
-
 function watching() {
-    watch(['app/scss/**/*.scss'], styles);
-    watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts)
-    watch(['app/*.html']).on('change', browserSync.reload);
+  watch(['app/scss/**/*.scss'], styles);
+  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+  watch(['app/*.html']).on('change', browserSync.reload);
 }
 
 exports.styles = styles;
@@ -89,5 +81,4 @@ exports.images = images;
 exports.cleanDist = cleanDist;
 
 exports.build = series(cleanDist, images, build);
-exports.default = parallel(styles, scripts, browsersync, watching)
-
+exports.default = parallel(styles, scripts, browsersync, watching);
